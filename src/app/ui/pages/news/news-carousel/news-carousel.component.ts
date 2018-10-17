9420d66227a674689414cf3ad6e0656f3f2186fd
Aob BKK <aob.wiriyagulpat@dx1app.com>
2018-10-17 10:53:25 +0700

    add zoom image when click (LightBox) Fixed #54

--- src/app/ui/pages/news/news-carousel/news-carousel.component.ts
+++ src/app/ui/pages/news/news-carousel/news-carousel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { NewsService } from '../../../../core/news.service';
+import { Lightbox, IAlbum } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-news-carousel',
@@ -14,7 +15,8 @@ export class NewsCarouselComponent implements OnInit {
 
   constructor(
     config: NgbCarouselConfig,
-    private newsService: NewsService
+    private newsService: NewsService,
+    private lightbox: Lightbox
   ) {
     config.interval = 5000;
     config.showNavigationArrows = false;
@@ -22,6 +24,20 @@ export class NewsCarouselComponent implements OnInit {
 
   get newsItems() {
     return this.newsService.previewItems;
+  }
+
+  openImage(imageUrl: string, caption: string) {
+    const option: IAlbum = {
+      src: imageUrl,
+      caption: caption,
+      thumb: ''
+    };
+    this.lightbox.open([option], 0);
+  }
+
+  close(): void {
+    // close lightbox programmatically
+    this.lightbox.close();
   }
 
   ngOnInit() {
