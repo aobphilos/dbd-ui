1389cc1ca15dea150e95053e3362889491e8dbe8
Aob BKK <aob.wiriyagulpat@dx1app.com>
2018-11-01 20:38:07 +0700

    update data 2018-11-01, add image url for Store

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Member } from '../model/member';
 import { MemberType } from '../enum/member-type';
 import { MemberService } from './member.service';
 import { auth } from 'firebase';
-import { delay } from 'q';
+import { StoreService } from './store.service';
+import { Store } from '../model/store';
+import { OwnerView } from '../model/views/owner-view';
 
 @Injectable({
   providedIn: 'root'
@@ -20,26 +22,27 @@ export class DataService {
 
   constructor(
     private http: HttpClient,
-    private memberService: MemberService) {
+    private memberService: MemberService,
+    private storeService: StoreService) {
     this.prepareMembers();
   }
 
 
 
-  upload() {
+  async upload() {
     const password = '123123';
 
     const addMember = async members => {
       if (members && members.length > 0) {
         members.forEach(async m => {
           // await delay(await this.createUserByEmail(m.email, password), 2000);
-          // await this.addToDb(m);
+          await this.addToDb(m);
         });
       }
     };
 
-    this.memberRetails.subscribe(addMember);
-    this.memberWholeSales.subscribe(addMember);
+    // await this.memberRetails.subscribe(addMember);
+    // await this.memberWholeSales.subscribe(addMember);
 
   }
 
@@ -51,6 +54,7 @@ export class DataService {
         const data = response['data'] as Member[];
         data.forEach(m => {
           m.memberType = type;
+
           members.push(Object.assign(new Member(), m));
         });
       }
@@ -69,7 +73,18 @@ export class DataService {
   }
 
   private async addToDb(member: Member) {
-    await this.memberService.add(member);
+    const imageUrl = member['imageUrl'];
+    delete member['imageUrl'];
+    const memberId = await this.memberService.add(member);
+
+    delay(2000);
+
+    const store = new Store();
+    store.ownerId = memberId;
+    store.owner = OwnerView.create(member);
+    store.imageUrl = imageUrl;
+    await this.storeService.add(store);
+
     console.log(`create user-data: ${member.email} - complete`);
   }
 
